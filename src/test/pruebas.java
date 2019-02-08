a8b2b552c55fb3dc49b152229207aca59911b888
monci07 <45374916+monci07@users.noreply.github.com>
2019-02-08 10:12:40 -0800

    Update pruebas.java

--- src/test/pruebas.java
+++ src/test/pruebas.java
@@ -6,7 +6,9 @@ public class pruebas {
         paquete1.Persona p = new paquete1.Persona(25,"maria");
         paquete2.Persona m = new paquete2.Persona(20,"Jose","masculino");
 
+        System.out.println("inicio de impresion");
         System.out.println(p.toString());
         System.out.println(m.toString());
+        System.out.println("Fin");
     }
 }
